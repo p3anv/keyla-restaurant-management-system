74dd6587ad1b65acc9788ef22e67867f2a48b186
Pranav Kamble <pranavkamble164@gmail.com>
2026-07-03 14:19:24 +0530

    Change jwt import to use namespace import syntax

--- apps/api/src/middleware/auth.ts
+++ apps/api/src/middleware/auth.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
 import { env } from '../config/env';
 import { prisma } from '../config/database';
 
