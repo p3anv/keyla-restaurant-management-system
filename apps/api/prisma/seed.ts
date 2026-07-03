81f279c2d5709fe9d38c442ba0ec3de07c19f75c
Pranav Kamble <pranavkamble164@gmail.com>
2026-07-03 14:48:32 +0530

    Change bcrypt import from 'bcrypt' to 'bcryptjs'

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient, UserRole, TableStatus, CourseType, TaxCategory } from '@prisma/client';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
